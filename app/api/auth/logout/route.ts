4314bae2c39f308cb68b66d805dfea07fa713fd1
apartmentsflora <contact.edelweisstudio@gmail.com>
2026-04-17 14:40:44 +0300

    Delete app directory

--- app/api/auth/logout/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  res.cookies.set("sp_session", "", { maxAge: 0, path: "/" });
-  return res;
-}
